b97e9065b611711ef8d4e31c249671a1ad064553
Osamah Bukraa <osamah.bukraa@digipolis.be>
2024-02-15 13:49:22 +0100

    Add test checking fromUrl logic, fix existing tests, add invalid url fallback

--- src/util/isValidCallbackUrl.js
+++ src/util/isValidCallbackUrl.js
@@ -1,10 +1,14 @@
 export function isValidCallbackUrl(url, allowedDomains = ['antwerpen.be']) {
-  const callbackUrl = new URL(url);
+  try {
+    const callbackUrl = new URL(url);
 
-  if (callbackUrl.protocol !== 'https:') {
+    if (callbackUrl.protocol !== 'https:') {
+      return false;
+    }
+
+    const regex = new RegExp(`(${allowedDomains.map((allowedDomain) => `${allowedDomain.replace('.', '\\.')}$`).join('|')})`);
+    return regex.test(callbackUrl.host);
+  } catch (error) {
     return false;
   }
-
-  const regex = new RegExp(`(${allowedDomains.map((allowedDomain) => `${allowedDomain.replace('.', '\\.')}$`).join('|')})`);
-  return regex.test(callbackUrl.host);
 }
